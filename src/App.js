426960c563c3095044346984917a954f70180ecc
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-20 23:41:48 -0500

    move Pet component into its own file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import Pet from "./components/Pet.js"
 
 const App = () => {
   return React.createElement("div", {}, [
