9be958ab113b2d2fa3acb02859a0acbc53e8f9e3
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-22 13:28:03 -0500

    handling user input in form, preventDefault

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -26,7 +26,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
