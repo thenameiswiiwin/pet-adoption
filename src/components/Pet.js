15a695cf62da2db9dc05f57f3a961e6c37a5c007
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-26 12:20:38 -0500

    added hero

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,10 +1,19 @@
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ id, images, name, animal, breed, location }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) hero = images[0];
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} — {breed} — {location}
+        </h2>
+      </div>
+    </a>
   );
 };
 
