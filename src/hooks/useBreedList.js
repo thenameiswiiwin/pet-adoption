c4e9602c3dcec5e550ffcfdd2ec71d405c9c7a71
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-26 12:20:55 -0500

    update

--- src/hooks/useBreedList.js
+++ src/hooks/useBreedList.js
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const localCache = {};
 
 export default function useBreedList(animal) {
   const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState('unloaded');
+  const [status, setStatus] = useState("unloaded");
 
   useEffect(() => {
     if (!animal) {
@@ -17,14 +17,14 @@ export default function useBreedList(animal) {
 
     async function requestBreedList() {
       setBreedList([]);
-      setStatus('loading');
+      setStatus("loading");
       const res = await fetch(
         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
       );
       const json = await res.json();
       localCache[animal] = json.breeds || [];
       setBreedList(localCache[animal]);
-      setStatus('loaded');
+      setStatus("loaded");
     }
   }, [animal]);
 
