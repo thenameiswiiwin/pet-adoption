1320e67fb2b078fbfd58f41d7dd3450840986065
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-26 13:25:25 -0500

    replaced h1 tag to Link inside header tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./components/SearchParams";
 import Details from "./components/Details";
 
@@ -8,7 +8,9 @@ const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
-        <h1>Adopt Me!</h1>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
         <Routes>
           <Route path="/" element={<SearchParams />} />
           <Route path="/details/:id" element={<Details />} />
