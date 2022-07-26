23614973553f8f2d3b37cc105b1a11012c74047d
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-26 12:59:03 -0500

    added ReactRouter, with home and details/:id routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { StrictMode } from 'react';
-import { render } from 'react-dom';
-import SearchParams from './components/SearchParams';
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchParams from "./components/SearchParams";
+import Details from "./components/Details";
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
+      <BrowserRouter>
         <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
