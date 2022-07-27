55ff885a767dec5100b9dea64475522b9f005791
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-27 17:38:49 -0500

    imported Carousel component, passed in the images prop, and display it on the page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
-import { Component } from "react";
-import { useParams } from "react-router-dom";
+import { Component } from 'react';
+import { useParams } from 'react-router-dom';
+import Carousel from './Carousel';
 
 class Details extends Component {
   state = { loading: true };
@@ -14,10 +15,11 @@ class Details extends Component {
 
   render() {
     if (this.state.loading) <h2>loading ... </h2>;
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } = this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
