24cdeb4472b3a20dbcdeabe797ac32221a5689b8
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-27 17:03:04 -0500

    trying out class component, with life CycleHook in Details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,42 @@
-import { useParams } from "react-router-dom";
+import { Component } from 'react';
+import { useParams } from 'react-router-dom';
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id}</h2>;
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) <h2>loading ... </h2>;
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} — {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
