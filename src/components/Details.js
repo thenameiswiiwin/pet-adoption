f20aacf3c90e4b51efe267e34eed5c1c3ac2b2c2
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-27 19:06:56 -0500

    created a ErrorBoundary component to catch errors with APIs. ErrorBoundary live above Details component.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,7 @@
-import { Component } from 'react';
-import { useParams } from 'react-router-dom';
-import Carousel from './Carousel';
+import { Component } from "react";
+import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -15,7 +16,8 @@ class Details extends Component {
 
   render() {
     if (this.state.loading) <h2>loading ... </h2>;
-    const { animal, breed, city, state, description, name, images } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
@@ -35,7 +37,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
