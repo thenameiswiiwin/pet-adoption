7c105f29b8ff11a83e64a13e475fa3d4bb7f01d7
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-07-28 13:33:00 -0500

    finished ErrorBoundary component

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Component } from "react";
+import { Link, Navigate } from "react-router-dom";
 
 class ErrorBoundary extends Component {
   state = { hasError: false, redirect: false };
@@ -23,7 +23,7 @@ class ErrorBoundary extends Component {
     } else if (this.state.hasError) {
       return (
         <h2>
-          There was an error with this listing. <Link to="/">Click here</Link>{' '}
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
           to back to the home page or wait five seconds.
         </h2>
       );
