bccea4ad492ab969554f2f3d7a1982f0a02064a7
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-11-17 00:00:18 -0600

    Pet component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import Pet from './Pet';
+
+const App = () => {
   return (
-    <div>
+    <div
+      className="m-0 p-0"
+      style={{
+        background: 'url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)',
+      }}
+    >
       <h1>Hello</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Pet.tsx
@@ -0,0 +1,17 @@
+interface PetProps {
+  name: string;
+  animal: string;
+  breed: string;
+}
+
+const Pet = ({ name, animal, breed }: PetProps) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+};
+
+export default Pet;
